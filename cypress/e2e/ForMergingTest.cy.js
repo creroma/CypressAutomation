ef00ebf931870c9cdedb483fcdf93a69de5ee4fb
creroma <reromacharles8@gmail.com>
2023-04-11 17:24:17 +0800

    add changes

--- cypress/e2e/ForMergingTest.cy.js
+++ cypress/e2e/ForMergingTest.cy.js
@@ -1,5 +1,42 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="Cypress"/>
+
+describe('For Merging test conflicts', () => {
+
+  beforeEach(() => {
+    cy.visit('https://rahulshettyacademy.com/client')
+      .url()
+      .should('include','/login')
+
   })
-})
+
+  function ForgotPass(email, password,confPassFP)
+    {
+
+        cy.get('input[placeholder="Enter your email address"]').type(email);
+        cy.get('#userPassword').type(password);
+        cy.get('#confirmPassword').type(confPassFP)
+        cy.get('button[type="submit"]').click();
+
+    }
+
+    it('Validations on forgot password', () => {
+      
+      cy.get('.forgot-password-link').click()  
+      ForgotPass('qatesting1','Testing1','Testing')
+      cy.get('.invalid-feedback')
+        .should('contain','Password and Confirm Password must match with each other.')
+      cy.get('.invalid-feedback')
+        .should('contain','*Enter Valid Email')
+    });
+
+    it.only('Clicking save without data', () => {
+
+      cy.get('.forgot-password-link').click()
+
+      cy.get('.btn-custom').click()
+
+      cy.get('.invalid-feedback').should('have.length',3)
+      
+    })
+    
+  })
