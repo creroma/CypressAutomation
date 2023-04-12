04edb2b350ec01ebd2924a66d4e4fd3eed9f37d6
creroma <reromacharles8@gmail.com>
2023-04-12 16:02:42 +0800

    Changes13

--- cypress/e2e/ForMergingTest.cy.js
+++ cypress/e2e/ForMergingTest.cy.js
@@ -29,7 +29,7 @@ describe('For Merging test conflicts', () => {
         .should('contain','*Enter Valid Email')
     });
 
-    it.only('Clicking save without data', () => {
+    it('Clicking save without data, new changes', () => {
 
       cy.get('.forgot-password-link').click()
 
